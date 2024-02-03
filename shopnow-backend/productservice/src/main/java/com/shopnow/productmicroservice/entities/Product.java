dc35707066a63271053488bacefb81281c895133
Priyanshu Anand <anandpriyanshu6@gmail.com>
2024-02-04 04:35:51 +0530

    Upgraded Product Microservices to 3.2.2 version

--- shopnow-backend/productservice/src/main/java/com/shopnow/productmicroservice/entities/Product.java
+++ shopnow-backend/productservice/src/main/java/com/shopnow/productmicroservice/entities/Product.java
@@ -1,17 +1,15 @@
 package com.shopnow.productmicroservice.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
